ca663be90b1503d0ed2029d9f6223bb4cb21dba4
Adrián García <adriagtrias@gmail.com>
2022-07-05 17:53:37 +0200

    Select correctly modplayer and clean up code

--- GlobalNPC.cs
+++ GlobalNPC.cs
@@ -8,20 +8,14 @@ namespace Infinitum
 {
 	public class InfinitumNPCs : GlobalNPC
 	{
-
 		public override bool CheckDead(NPC npc)
 		{
 			if (base.CheckDead(npc))
 			{
-		 		//Mod infinitumMod = ModLoader.GetMod("Infinitum");
 				float calcExp = (float)npc.defense + 0.5f * (float)(npc.lifeMax / 5);
-				//Infinitum.PlayerModded.AddXp(calcExp);
-				//Character_Data mp = (Character_Data)ModContent.GetInstance<Character_Data>();
-				//Main.player[Main.myPlayer].GetModPlayer<Character_Data>().AddXp(calcExp);
-				//Character_Data.ModPlayer(Main.player[Main.myPlayer]).AddXp(calcExp);
-				//mp.AddXp(calcExp);
-				
-				Character_Data.AddXp(calcExp);
+
+				Main.player[Main.myPlayer].GetModPlayer<Character_Data>().AddXp(calcExp);
+
 				return true;
 			}
 			return false;
@@ -30,11 +24,12 @@ namespace Infinitum
 		{
 			System.Collections.Generic.List<IItemDropRule> items = new System.Collections.Generic.List<IItemDropRule>();
 			items.Add(new CommonDrop(ModContent.ItemType<ExpStar>(), 1500));
-			items.Add(new CommonDrop(ModContent.ItemType<MultiplierStar>(), 250,1,3));
-			
+			items.Add(new CommonDrop(ModContent.ItemType<MultiplierStar>(), 250, 1, 3));
+
 			items.ForEach(e => npcLoot.Add(e));
 			base.ModifyNPCLoot(npc, npcLoot);
-			
+
 		}
+
 	}
 }
--- Infinitum.cs
+++ Infinitum.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 namespace Infinitum
 {
 	public class Infinitum : Mod
-	{	
-		
+	{
+		public Infinitum()
+		{
+
+		}
+		public override void Load()
+		{
+			base.Load();
+			
+		}
 	}
 }
