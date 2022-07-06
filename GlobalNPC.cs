b261cef2b92c0a251adbdc015ec750a39834b4e8
Adrián García <adriagtrias@gmail.com>
2022-07-06 04:26:29 +0200

    check netmode for future.

--- GlobalNPC.cs
+++ GlobalNPC.cs
@@ -1,8 +1,10 @@
 using Terraria;
+using Terraria.Chat;
 using Terraria.ModLoader;
 using Infinitum.Items;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.Localization;
+using Microsoft.Xna.Framework;
 
 namespace Infinitum
 {
@@ -16,14 +18,26 @@ namespace Infinitum
 		// 	float calcExp = (float)npc.defense + 0.5f * (float)(npc.lifeMax / 5);
 
 		// 	Main.player[Main.myPlayer].GetModPlayer<Character_Data>().AddXp(calcExp);
-		// }W
+		// }
 		public override void OnKill(NPC npc)
 		{
 			base.OnKill(npc);
 			addXpToAllPlayerInRange((float)npc.defense + 0.5f * (float)(npc.lifeMax / 5));
-
+			ChatMessage("xd");
 		}
-		
+		public static void ChatMessage(string text)
+		{
+			
+			if (Main.netMode == 2)
+			{
+				ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(text), Color.Red);
+				
+			}
+			else if (Main.netMode == 0)
+			{
+				Main.NewText(text);
+			}
+		}
 		public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
 		{
 			System.Collections.Generic.List<IItemDropRule> items = new System.Collections.Generic.List<IItemDropRule>();
@@ -43,7 +57,7 @@ namespace Infinitum
 				//if(Main.netMode == NetmodeID.Server)
 				//Main.NewText(Main.PlayerList.ToString());		
 
-				NetMessage.SendData(1, -1, -1, NetworkText.FromLiteral("testing"));
+				//NetMessage.SendData(1, -1, -1, NetworkText.FromLiteral("testing"));
 
 				p.GetModPlayer<Character_Data>().AddXp(xp);
 
