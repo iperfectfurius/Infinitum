b030d9a78ad7b33da5b460ef503bd1fae9aff08c
Adrián García <adriagtrias@gmail.com>
2022-07-18 18:55:29 +0200

    Added 'xp description' to tooltip weapon when Modprefix of infinitum.

---
+++ Items/InfinitumGlobalItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Infinitum.Items
+{
+    internal class InfinitumGlobalItem : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+
+            ModPrefix prefix = PrefixLoader.GetPrefix(item.prefix);
+            if(prefix != null)
+            {
+                switch (prefix.Name)
+                {
+                    case "UnrealPlus":
+                    case "MythicalPlus":
+                    case "LegendaryPlus":
+                        tooltips.Insert(6, new TooltipLine(Mod, "Inifnitum", "[c/FF0000:+ 25% Global XP!]"));
+                        
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+}
